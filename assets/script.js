2702bfd7eb228720a2f49b2f161ebe3ddc83d423
DoengesBA <110855912+DoengesBA@users.noreply.github.com>
2023-02-08 17:42:05 -0700

    Timeline in JS such as Past/Future or Present

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,20 @@ $(document).ready(function() {
     let displayDate = document.getElementById("currentDay");
     displayDate.innerHTML = now;
     let currentHour = moment().format("HH");
+    
+    $(".time-div").each(function() {
+        var timeDiv = $(this).attr("id").split("-")[1];
+
+        if (currentHour == timeDiv) {
+            $(this).addClass("present");
+            $(this).children(".description").addClass("present");
+   
+        } else if (currentHour < timeDiv) {
+            $(this).removeClass("present");
+            $(this).addClass("future");
+
+        } else if (currentHour > timeDiv) {
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
